4c956edaff75847bf5e166eb8f0523c6989fee29
Stewart Campbell <stewartcuk@gmail.com>
2015-06-12 10:31:13 +0100

    First pass at project

---
+++ server.js
@@ -0,0 +1,41 @@
+/*jshint unused:true */
+var express = require('express');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+var morgan = require('morgan');
+
+var mongoose = require('mongoose');
+
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+// Configure to handle CORS requests
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+  res.send('Welcome to the home page!');
+});
+
+var apiRouter = express.Router();
+apiRouter.get('/', function(req, res){
+  res.json({ message: 'welcome to teh api!' });
+});
+app.use('/api', apiRouter);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
