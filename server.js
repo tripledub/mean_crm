f8ef299f41d0b2074b1e071e6b589f4ad659fdf0
Stewart Campbell <stewartcuk@gmail.com>
2015-10-28 20:56:05 +0000

    jwt token authentication

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var morgan = require('morgan');
 
+var jwt = require('jsonwebtoken');
+var superSecret = 'clockworkcryingbarcode6inarow';
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/meanCRM');
 
@@ -33,9 +36,67 @@ app.get('/', function(req, res){
 });
 
 var apiRouter = express.Router();
+
+apiRouter.post('/authenticate', function(req, res) {
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.json({
+        success: false,
+        message: 'Authentication failed. User not found.'
+      });
+    } else if (user) {
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. Wrong password'
+        });
+      } else {
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresIn: 1440 * 60
+        });
+
+        res.json({
+          success: true,
+          message: 'Enjoy your token',
+          token: token
+        });
+      }
+    }
+  });
+});
+
 apiRouter.use(function(req, res, next) {
   console.log('Someone hit da app!!');
-  next();
+  var token = req.body.token ||
+              req.query.token ||
+              req.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, superSecret, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token'
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+  // next();
 });
 
 apiRouter.get('/', function(req, res){
