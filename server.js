5cef138ac221308a0e8e0563a1a698b967e2fea9
Stewart Campbell <stewartcuk@gmail.com>
2015-10-28 17:39:46 +0000

    User CRUD

--- server.js
+++ server.js
@@ -35,7 +35,6 @@ app.get('/', function(req, res){
 var apiRouter = express.Router();
 apiRouter.use(function(req, res, next) {
   console.log('Someone hit da app!!');
-
   next();
 });
 
@@ -44,7 +43,64 @@ apiRouter.get('/', function(req, res){
 });
 app.use('/api', apiRouter);
 
+
 var User = require('./app/models/user.js');
+
+apiRouter.route('/users')
+  .post(function(req, res) {
+    var user = new User();
+
+    user.name     = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    user.save(function(err) {
+      if (err) {
+        // duplicate entry
+        if (err.code == 11000) {
+          return res.json( { success: false, message: 'User with that name exists' } );
+        } else {
+          return res.send(err);
+        }
+      }
+
+      res.json({ message: 'User created' });
+    });
+  })
+  .get(function(req, res){
+    User.find(function(err, users){
+      if (err) res.send(err);
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) res.send(err);
+      res.json(user);
+    });
+  })
+  .put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) res.send(err);
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.password) user.password = req.body.password;
+      user.save(function(err, user) {
+        if (err) res.send(err);
+        res.json({ message: 'User updated' });
+      });
+    });
+  })
+  .delete(function(req, res) {
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if (err) return res.send(err);
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
