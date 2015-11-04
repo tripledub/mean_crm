6a9a27005eea08e0480d8a36802f77a264857340
Stewart Campbell <stewartcuk@gmail.com>
2015-11-04 09:56:00 +0000

    Refatoring / app layout

---
+++ app/routes/api.js
@@ -0,0 +1,140 @@
+var User   = require('../models/user');
+var jwt    = require('jsonwebtoken');
+var config = require('../../config');
+
+var superSecret = config.secret;
+
+module.exports = function(app, express) {
+  var apiRouter = express.Router();
+
+  apiRouter.post('/authenticate', function(req, res) {
+    User.findOne({
+      username: req.body.username
+    }).select('name username password').exec(function(err, user) {
+      if (err) throw err;
+
+      if (!user) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. User not found.'
+        });
+      } else if (user) {
+        var validPassword = user.comparePassword(req.body.password);
+        if (!validPassword) {
+          res.json({
+            success: false,
+            message: 'Authentication failed. Wrong password'
+          });
+        } else {
+          var token = jwt.sign({
+            name: user.name,
+            username: user.username
+          }, superSecret, {
+            expiresIn: 1440 * 60
+          });
+
+          res.json({
+            success: true,
+            message: 'Enjoy your token',
+            token: token
+          });
+        }
+      }
+    });
+  });
+
+  apiRouter.use(function(req, res, next) {
+    console.log('Someone hit da app!!');
+    var token = req.body.token ||
+      req.query.token ||
+      req.headers['x-access-token'];
+    if (token) {
+      jwt.verify(token, superSecret, function(err, decoded) {
+        if (err) {
+          return res.status(403).send({
+            success: false,
+            message: 'Failed to authenticate token'
+          });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      });
+    }
+    // next();
+  });
+
+  apiRouter.get('/', function(req, res){
+    res.json({ message: 'welcome to teh api!' });
+  });
+
+
+  // var User = require('./app/models/user.js');
+
+  apiRouter.route('/users')
+    .post(function(req, res) {
+      var user = new User();
+
+      user.name     = req.body.name;
+      user.username = req.body.username;
+      user.password = req.body.password;
+
+      user.save(function(err) {
+        if (err) {
+          // duplicate entry
+          if (err.code == 11000) {
+            return res.json( { success: false, message: 'User with that name exists' } );
+          } else {
+            return res.send(err);
+          }
+        }
+
+        res.json({ message: 'User created' });
+      });
+    })
+  .get(function(req, res){
+    User.find(function(err, users){
+      if (err) res.send(err);
+      res.json(users);
+    });
+  });
+
+  apiRouter.route('/users/:user_id')
+    .get(function(req, res) {
+      User.findById(req.params.user_id, function(err, user) {
+        if (err) res.send(err);
+        res.json(user);
+      });
+    })
+  .put(function(req, res) {
+    User.findById(req.params.user_id, function(err, user) {
+      if (err) res.send(err);
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.username) user.username = req.body.username;
+      if (req.body.password) user.password = req.body.password;
+      user.save(function(err, user) {
+        if (err) res.send(err);
+        res.json({ message: 'User updated' });
+      });
+    });
+  })
+  .delete(function(req, res) {
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user) {
+      if (err) return res.send(err);
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
+  apiRouter.get('/me', function(req, res) {
+    res.send(req.decoded);
+  });
+
+  return apiRouter;
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 /*jshint unused:vars */
+var config = require('./config');
+
 var express = require('express');
 
 var app = express();
@@ -7,13 +9,8 @@ var bodyParser = require('body-parser');
 
 var morgan = require('morgan');
 
-var jwt = require('jsonwebtoken');
-var superSecret = 'clockworkcryingbarcode6inarow';
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/meanCRM');
-
-var port = process.env.PORT || 8080;
+mongoose.connect(config.database);
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -31,140 +28,17 @@ app.use(function(req, res, next){
 
 app.use(morgan('dev'));
 
-app.get('/', function(req, res){
-  res.send('Welcome to the home page!');
+var path = require('path');
+
+var apiRoutes = require('./app/routes/api') (app, express);
+app.use('/api', apiRoutes);
+
+// MAIN CATCHALL ROUTE --------
+// SEND USERS TO FRONTEND
+// must be registerd after API Routes
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
-var apiRouter = express.Router();
-
-apiRouter.post('/authenticate', function(req, res) {
-  User.findOne({
-    username: req.body.username
-  }).select('name username password').exec(function(err, user) {
-    if (err) throw err;
-
-    if (!user) {
-      res.json({
-        success: false,
-        message: 'Authentication failed. User not found.'
-      });
-    } else if (user) {
-      var validPassword = user.comparePassword(req.body.password);
-      if (!validPassword) {
-        res.json({
-          success: false,
-          message: 'Authentication failed. Wrong password'
-        });
-      } else {
-        var token = jwt.sign({
-          name: user.name,
-          username: user.username
-        }, superSecret, {
-          expiresIn: 1440 * 60
-        });
-
-        res.json({
-          success: true,
-          message: 'Enjoy your token',
-          token: token
-        });
-      }
-    }
-  });
-});
-
-apiRouter.use(function(req, res, next) {
-  console.log('Someone hit da app!!');
-  var token = req.body.token ||
-              req.query.token ||
-              req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, superSecret, function(err, decoded) {
-      if (err) {
-        return res.status(403).send({
-          success: false,
-          message: 'Failed to authenticate token'
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  } else {
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.'
-    });
-  }
-  // next();
-});
-
-apiRouter.get('/', function(req, res){
-  res.json({ message: 'welcome to teh api!' });
-});
-app.use('/api', apiRouter);
-
-
-var User = require('./app/models/user.js');
-
-apiRouter.route('/users')
-  .post(function(req, res) {
-    var user = new User();
-
-    user.name     = req.body.name;
-    user.username = req.body.username;
-    user.password = req.body.password;
-
-    user.save(function(err) {
-      if (err) {
-        // duplicate entry
-        if (err.code == 11000) {
-          return res.json( { success: false, message: 'User with that name exists' } );
-        } else {
-          return res.send(err);
-        }
-      }
-
-      res.json({ message: 'User created' });
-    });
-  })
-  .get(function(req, res){
-    User.find(function(err, users){
-      if (err) res.send(err);
-      res.json(users);
-    });
-  });
-
-apiRouter.route('/users/:user_id')
-  .get(function(req, res) {
-    User.findById(req.params.user_id, function(err, user) {
-      if (err) res.send(err);
-      res.json(user);
-    });
-  })
-  .put(function(req, res) {
-    User.findById(req.params.user_id, function(err, user) {
-      if (err) res.send(err);
-      if (req.body.name) user.name = req.body.name;
-      if (req.body.username) user.username = req.body.username;
-      if (req.body.password) user.password = req.body.password;
-      user.save(function(err, user) {
-        if (err) res.send(err);
-        res.json({ message: 'User updated' });
-      });
-    });
-  })
-  .delete(function(req, res) {
-    User.remove({
-      _id: req.params.user_id
-    }, function(err, user) {
-      if (err) return res.send(err);
-      res.json({ message: 'Successfully deleted' });
-    });
-  });
-apiRouter.get('/me', function(req, res) {
-  res.send(req.decoded);
-});
-
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
